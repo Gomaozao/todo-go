5ede1be8d2d77ca46001d2dd44acf4cd6daaa88d
Gomaozao <40327219+Gomaozao@users.noreply.github.com>
2025-11-08 17:06:48 -0300

    feat: print todo functionality

--- todo.go
+++ todo.go
@@ -3,7 +3,11 @@ package main
 import (
 	"errors"
 	"fmt"
+	"os"
+	"strconv"
 	"time"
+
+	"github.com/aquasecurity/table"
 )
 
 type Todo struct {
@@ -42,4 +46,48 @@ func (todos *todos) deleteTodo(index int) error {
 	}
 	*todos = append(t[:index], t[index+1:]...)
 	return nil
+}
+
+// method that takes an index as input and turns whatever
+func (todos *todos) toggle(index int) error {
+	t := *todos
+	if err := t.validadeIndex(index); err != nil {
+		return err
+	}
+	completed := t[index].isCompleted
+	if !completed {
+		completionTime := time.Now()
+		t[index].completedAt = &completionTime
+	}
+	t[index].isCompleted = !completed
+	return nil
+}
+
+func (todos *todos) edit(index int, title string) error {
+	t := *todos
+	if err := t.validadeIndex(index); err != nil {
+		return err
+	}
+	t[index].todoName = title
+
+	return nil
+}
+
+func (todos *todos) print() {
+	table := table.New(os.Stdout)
+	table.SetRowLines(false)
+	table.SetHeaders("#", "TodoName", "isCompleted", "CreatedAt", "CompletedAt")
+	for index, t := range *todos {
+		completed := "❌"
+		completedAt := ""
+
+		if t.isCompleted {
+			completed = "✅"
+			if t.completedAt != nil {
+				completedAt = t.completedAt.Format(time.RFC1123)
+			}
+		}
+		table.AddRow(strconv.Itoa(index), t.todoName, completed, t.createdAt.Format(time.RFC1123), completedAt)
+	}
+	table.Render()
 }
